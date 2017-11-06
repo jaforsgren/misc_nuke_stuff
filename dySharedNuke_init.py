5e732d2d19d9b0b54fe557f4673057f1b08a3644
Johan <jfo@dynagraph.se>
2017-11-06 13:52:36 +0100

    First commit

---
+++ dySharedNuke_init.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+import subprocess
+import sys
+import os.path as path
+import nuke
+
+###adding pluginpaths for rapid images globals###
+
+
+
+
+
+nuke.pluginAddPath('//DISKSTATION/Projects/_Admin/Lokaler_utrustning_can/Datorer_natverk/shared/workgroups/nuke/globals/')
+nuke.pluginAddPath('//DISKSTATION/Projects/_Admin/Lokaler_utrustning_can/Datorer_natverk/shared/workgroups/nuke/globals/gizmos')
+nuke.pluginAddPath('//DISKSTATION/Projects/_Admin/Lokaler_utrustning_can/Datorer_natverk/shared/workgroups/nuke/globals/templates')
+nuke.pluginAddPath('//DISKSTATION/Projects/_Admin/Lokaler_utrustning_can/Datorer_natverk/shared/workgroups/nuke/globals/python')
+
+nuke.pluginAddPath('//DISKSTATION/Projects/_Admin/Lokaler_utrustning_can/Datorer_natverk/shared/workgroups/nuke/ExternalTools/')
+nuke.pluginAddPath('//DISKSTATION/Projects/_Admin/Lokaler_utrustning_can/Datorer_natverk/shared/workgroups/nuke/ExternalTools/gizmos')
+nuke.pluginAddPath('//DISKSTATION/Projects/_Admin/Lokaler_utrustning_can/Datorer_natverk/shared/workgroups/nuke/ExternalTools/templates')
+nuke.pluginAddPath('//DISKSTATION/Projects/_Admin/Lokaler_utrustning_can/Datorer_natverk/shared/workgroups/nuke/ExternalTools/python')
+
+nuke.pluginAddPath('//DISKSTATION/Projects/_Admin/Lokaler_utrustning_can/Datorer_natverk/shared/workgroups/nuke/vikingbad/')
+nuke.pluginAddPath('//DISKSTATION/Projects/_Admin/Lokaler_utrustning_can/Datorer_natverk/shared/workgroups/nuke/vikingbad/gizmos')
+nuke.pluginAddPath('//DISKSTATION/Projects/_Admin/Lokaler_utrustning_can/Datorer_natverk/shared/workgroups/nuke/vikingbad/templates')
+nuke.pluginAddPath('//DISKSTATION/Projects/_Admin/Lokaler_utrustning_can/Datorer_natverk/shared/workgroups/nuke/vikingbad/python')
+
+nuke.tprint("successfully loaded dyNuke globals directory")
+
+nuke.pluginAddPath('//DISKSTATION/Projects/_Admin/Lokaler_utrustning_can/Datorer_natverk/shared/workgroups/nuke/globals/Tools/Icons')
+nuke.pluginAddPath('//DISKSTATION/Projects/_Admin/Lokaler_utrustning_can/Datorer_natverk/shared/workgroups/nuke/globals/Tools')
+
+
+###adding pluginpaths for comp gizmos from Nukepedia###
+nuke.pluginAddPath(
+    '//DISKSTATION/Projects/_Admin/Lokaler_utrustning_can/Datorer_natverk/shared/workgroups/nuke/globals/gizmos/Nukepedia')
+
